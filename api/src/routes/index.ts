d6e69dcbf01bd23f79bf3792d3c36365b314ad5c
Maurício Caminha <mauricio.caminha@hotmail.com>
2023-02-11 16:19:49 -0300

    JWT authentication Use Cases

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
+import { authenticateRoutes } from "./authenticate.routes";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
